070cfe4514f9375199dda00cff337bdc80c04091
c0deNeyt <chanchristianarana@gmail.com>
2023-04-30 12:45:07 +0800

    initial modification

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "UI Designer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+
+AOS.init({
+    offset: 400,
+    duration: 1000,
+});
