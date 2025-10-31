6ad7f5558faebf4eb07fbf9c41b66f858c192272
c0deNeyt <naitsircher@gmail.com>
2025-10-31 09:00:20 +0800

    Changes to be committed: 	modified:   css/style.css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-  strings: ["Web Developer", "Who", "Love LINUX"],
+  strings: ["Linux Enthusiast" , "Web Developer", "Love Vim"],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
@@ -10,18 +10,3 @@ AOS.init({
   duration: 1000,
 });
 
-// Get the button and menu elements
-const actionButton = document.getElementById("actionButton");
-const actionMenu = document.getElementById("actionMenu");
-
-// Toggle the menu visibility when the button is clicked
-actionButton.addEventListener("click", function (event) {
-  event.stopPropagation(); // Prevent event from bubbling
-  actionMenu.style.display =
-    actionMenu.style.display === "block" ? "none" : "block";
-});
-
-// Hide the menu when clicking outside of it
-document.addEventListener("click", function () {
-  actionMenu.style.display = "none";
-});
